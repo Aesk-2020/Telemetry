f310f83606d0a9002e97b8f8801f5e7c73a3c948
yelaydin14 <63052608+yelaydin14@users.noreply.github.com>
2020-04-21 19:41:32 +0300

    Yeni Tur Algoritması

---
+++ LyraTelemetryOldInterFaceNewTourAlgorithm/Telemetry/GPX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetry
+{
+    public class GPX
+    {
+        public struct StartCoordinate
+        {
+            double X { get; set; }
+            double Y { get; set; }
+        }
+
+        public struct EndCoordinate
+        {
+            double X { get; set; }
+            double Y { get; set; }
+        }
+    }
+}
