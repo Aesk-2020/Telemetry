cd6a069b9c12484821df253384ca9a165730eafd
YUSUF KEMAL PALACI <l1418051@std.yildiz.edu.tr>
2020-05-23 19:16:28 +0300

    gsfdfdsdgfsdsg

--- AeskApp/lib/pages/General.dart
+++ AeskApp/lib/pages/General.dart
@@ -1,5 +1,10 @@
 //Hız, süre, atılan tur, taranan açı
 
+<<<<<<< HEAD
+=======
+import 'package:aeskapp/custom_widgets/aesk_scaffold.dart';
+import 'package:aeskapp/custom_widgets/text_widget.dart';
+>>>>>>> YusufYıldız
 import 'package:flutter/material.dart';
 
 class General extends StatelessWidget {
