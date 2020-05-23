08b1de47e1c74d153bb77f02734bbaa89c8e1ff4
JosephStar318 <yusufyldz318@gmail.com>
2020-05-23 14:51:43 +0300

    Genel hatlar oluşturuldu

---
+++ AeskApp/lib/pages/Login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:aeskapp/custom_widgets/text_widget.dart';
+
+class Logging extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: myText("selamunaleyüm", 30, Colors.blue),
+      ),
+
+    );
+  }
+}
