6a287d14ac72fc33a6e06905c2e9742165355fcd
JosephStar318 <yusufyldz318@gmail.com>
2020-05-24 23:07:26 +0300

    Thema altyapısı eklendi Drawer düzenlendi. widgetler tek bir dosyada toplandı.

--- AeskApp/lib/pages/Custom.dart
+++ AeskApp/lib/pages/Custom.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:aeskapp/custom_widgets/front_inventory.dart';
+
+
 class Custom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Aesk_background,
-
 
     );
   }
--- AeskApp/lib/pages/Home.dart
+++ AeskApp/lib/pages/Home.dart
@@ -1,10 +1,11 @@
-import 'package:aeskapp/custom_widgets/aesk_scaffold.dart';
+import 'package:aeskapp/custom_widgets/aesk_widgets.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return aeskScaffold(
+
       myBody: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
