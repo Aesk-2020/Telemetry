61aebecf9971f342e6b961a83ef8724cc45cefec
JosephStar318 <yusufyldz318@gmail.com>
2020-05-26 15:05:03 +0300

    Provider desteği eklendi. Login ekranına başlandı.

--- AeskApp/lib/custom_widgets/front_inventory.dart
+++ AeskApp/lib/custom_widgets/front_inventory.dart
@@ -3,68 +3,63 @@ import 'package:flutter/material.dart';
 Color aeskBlue = Color.fromRGBO(47, 136, 202, 1.0);
 Color aeskBlack = Color.fromRGBO(48, 51, 58, 1.0);
 
-ThemeData MyTheme(isDarkMode) {
-  if (isDarkMode) {
-    ///Karanlık mod
-    return ThemeData(
-      backgroundColor: aeskBlack,
-      buttonColor: aeskBlue,
-      appBarTheme: AppBarTheme(
-        color: Colors.grey,
-        textTheme: TextTheme(
-          title: TextStyle(
-            color: Colors.white,
-            fontFamily: "GOTHIC",
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-
-
-          ),
-        ),
-      ),
-      textSelectionColor: Colors.blue,
+ThemeData LightTheme() {
+  return ThemeData(
+    // Aydınlık mod
+    backgroundColor: Colors.white70,
+    buttonColor: aeskBlue,
+    appBarTheme: AppBarTheme(
+      color: Colors.blueAccent,
       textTheme: TextTheme(
-          body1: TextStyle(
-            color: Colors.white,
-          ),
-          title: TextStyle(
-            color: Colors.amber,
-          ),
-          body2: TextStyle(
-            color: Colors.black,
-          ),
-      ),
-
-    );
-  } else {
-    return ThemeData(
-      // Aydınlık mod
-      backgroundColor: Colors.white70,
-      buttonColor: aeskBlue,
-      appBarTheme: AppBarTheme(
-        color: Colors.blueAccent,
-        textTheme: TextTheme(
-          title: TextStyle(
-            color: Colors.amber,
-            fontFamily: "GOTHIC",
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-
-          ),
-        ),
-      ),
-      textSelectionColor: Colors.blue,
-      textTheme: TextTheme(
-        body1: TextStyle(
-          color: Colors.black,
-        ),
         title: TextStyle(
           color: Colors.amber,
-        ),
-        body2: TextStyle(
-          color: Colors.white,
+          fontFamily: "GOTHIC",
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
         ),
       ),
-    );
-  }
+    ),
+    textSelectionColor: Colors.blue,
+    textTheme: TextTheme(
+      body1: TextStyle(
+        color: Colors.black,
+      ),
+      title: TextStyle(
+        color: Colors.amber,
+      ),
+      body2: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
+
+ThemeData DarkTheme() {
+  return ThemeData(
+    backgroundColor: aeskBlack,
+    buttonColor: aeskBlue,
+    appBarTheme: AppBarTheme(
+      color: Colors.grey,
+      textTheme: TextTheme(
+        title: TextStyle(
+          color: Colors.white,
+          fontFamily: "GOTHIC",
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    textSelectionColor: Colors.blue,
+    textTheme: TextTheme(
+      body1: TextStyle(
+        color: Colors.white,
+      ),
+      title: TextStyle(
+        color: Colors.amber,
+      ),
+      body2: TextStyle(
+        color: Colors.black,
+      ),
+    ),
+  );
 }
--- AeskApp/lib/pages/Home.dart
+++ AeskApp/lib/pages/Home.dart
@@ -1,23 +1,21 @@
+import 'package:aeskapp/classes/theme.dart';
 import 'package:aeskapp/custom_widgets/aesk_widgets.dart';
+import 'package:aeskapp/custom_widgets/front_inventory.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return aeskScaffold(
-
-      myBody: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            FlatButton(
-              onPressed: (){},
-              child: Text("deneme butonu"),
-            ),
-          ],
-        ),
-      ),
-      context: context,
+    return Consumer<MyThemeData>(
+      builder: (context, ourTheme, child) {
+        return aeskScaffold(
+          context: context,
+          myBody: Center(
+            child: Text((ourTheme.myTheme == DarkTheme()) ? "bu bir karanlık moddur" : "bu aydınlık moddur"),
+          ),
+        );
+      },
     );
   }
 }
