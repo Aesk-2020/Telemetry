252f3235b4802086d465831b0ae293b8564f6dcd
yelaydin14 <63052608+yelaydin14@users.noreply.github.com>
2020-06-01 14:59:00 +0300

    Keil Delete

--- Aesk_Lyra_Telemetry_Keil/MDK-ARM/Aesk_Lyra_Telemetry_Keil/aesk_data_pack_lib.d
+++
@@ -1,3 +0,0 @@
-aesk_lyra_telemetry_keil\aesk_data_pack_lib.o: ..\Src\AESK_Data_Pack_lib.c
-aesk_lyra_telemetry_keil\aesk_data_pack_lib.o: ../Inc/AESK_Data_Pack_Lib.h
-aesk_lyra_telemetry_keil\aesk_data_pack_lib.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Aesk_Lyra_Telemetry_Keil/MDK-ARM/Aesk_Lyra_Telemetry_Keil/startup_stm32f412rx.d
+++
@@ -1,1 +0,0 @@
-aesk_lyra_telemetry_keil\startup_stm32f412rx.o: startup_stm32f412rx.s
