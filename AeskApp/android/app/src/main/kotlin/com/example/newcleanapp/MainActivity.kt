263b4e8cba7461195d82e957bc9c75db556e8b7c
yusufkmll <44361007+yusufkmll@users.noreply.github.com>
2020-10-02 17:51:12 +0300

    temizlik

--- AeskApp/android/app/src/main/kotlin/com/example/newcleanapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newcleanapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- AeskApp/lib/classes/drawer_list_class.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawerListClass{
-  final String image;
-  final String text;
-  String destination = "";
-
-  DrawerListClass({@required this.image, @required this.text, this.destination});
-}
