c6130976ce0bb1ad622e135bf5cfb79957793328
yusufkmll <44361007+yusufkmll@users.noreply.github.com>
2021-03-01 16:07:33 +0300

    bms arayüzü güncellendi

--- Lyra/Telemetry - Electric/yeniform/NewForms/BMS.cs
+++ Lyra/Telemetry - Electric/yeniform/NewForms/BMS.cs
@@ -16,5 +16,10 @@ namespace Telemetri.NewForms
         {
             InitializeComponent();
         }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
