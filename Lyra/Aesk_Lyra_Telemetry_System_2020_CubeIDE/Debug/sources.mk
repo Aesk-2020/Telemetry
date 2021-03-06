e3e27bb1836bc29497d9d20ce22c24eb56a7a5b6
yusufkmll <44361007+yusufkmll@users.noreply.github.com>
2021-03-06 21:12:40 +0300

    float ayarı yapıldı

--- Lyra/Aesk_Lyra_Telemetry_System_2020_CubeIDE/Debug/sources.mk
+++ Lyra/Aesk_Lyra_Telemetry_System_2020_CubeIDE/Debug/sources.mk
@@ -20,7 +20,6 @@ OBJCOPY_BIN :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 Core/Src \
-Core/Startup \
 Drivers/STM32F4xx_HAL_Driver/Src \
 FATFS/App \
 FATFS/Target \
